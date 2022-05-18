82b6b3525756418a70714415844389219fff643a
emiliero <emilie.rosnas@hotmail.com>
2022-05-18 14:29:17 +0200

    creating client subscription workflow with push service

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 const client = (() => {
     let serviceWorkerRegObj = undefined;
     const notificationButton = document.getElementById("btn-notify");
+    const pushButton = document.getElementById("btn-push");
+    let isUserSubscribed = false;
 
     const showNotificationButton = () => {
         notificationButton.style.display = "block";
@@ -49,6 +51,12 @@ const client = (() => {
                 console.log("service worker is registered successfully!")
                 serviceWorkerRegObj = regObj;
                 showNotificationButton();
+
+                serviceWorkerRegObj.pushManager.getSubscription()
+                    .then(subs => {
+                        if (subs) disablePushNotificationButton()
+                        else enablePushNotificationButton()
+                    });
             });
     }
 
@@ -61,5 +69,60 @@ const client = (() => {
     checkNotificationSupport()
         .then(registerServiceWorker)
         .then(requestNotificationPermissions)
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
+
+    const disablePushNotificationButton = () => {
+        isUserSubscribed = true;
+        pushButton.innerText = "DISABLE PUSH NOTIFICATIONS";
+        pushButton.style.backgroundColor = "#EA9085";
+    }
+
+    const enablePushNotificationButton = () => {
+        isUserSubscribed = false;
+        pushButton.innerText = "ENABLE PUSH NOTIFICATIONS";
+        pushButton.style.backgroundColor = "#EFB1FF";
+    }
+
+    const setupPush = () => {
+        function url864ToUint8Array(url) {
+            const padding = '='.repeat((4 - url.length % 4) % 4);
+            const base64 = (url + padding).replace(/\-/g, '+').replace(/_/g, '/');
+
+            const rawData = window.atob(base64);
+            const outputArray = new Uint8Array(rawData.length);
+
+            for (let i = 0; i < rawData.length; i++) {
+                outputArray[i] = rawData.charCodeAt(i);
+            }
+            return outputArray;
+        }
+
+        const subscribeUser = () => {
+            const appServerPublicKey = "BMMZ-619ACjxnbh1fwAPYsrAtEXPZ8YyoIGPHyqGfU7iTYTg45RWjKPzorG1_wP1WW5Y9SkEm4lHUyJv4wg5pHw";
+            const publicKeyAsArray = url864ToUint8Array(appServerPublicKey);
+
+            serviceWorkerRegObj.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: publicKeyAsArray
+            }).then(subscription => {
+                console.log(JSON.stringify(subscription, null, 4));
+                disablePushNotificationButton();
+            }).catch(error => console.error("Failed to ubscribe to Push Service", error))
+        }
+        const unSubscribeUser = () => {
+            console.log("un-subscribing user")
+            serviceWorkerRegObj.pushManager.getSubscription()
+                .then(subscription => {
+                    if (subscription) return subscription.unsubscribe();
+                })
+                .then(enablePushNotificationButton())
+                .catch(error => console.error("Failed ot unsubscribe from Push Service", error))
+        }
+
+        pushButton.addEventListener("click", () => {
+            if (isUserSubscribed) unSubscribeUser();
+            else subscribeUser(); 
+        })
+    }
+    setupPush();
 })();
